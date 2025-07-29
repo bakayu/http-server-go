9fc529a4539228ee6701fc2a9039002531bc7c06
Ayush <mail@ayuch.dev>
2025-07-29 14:28:18 +0530

    feat: add in-memory player store
    Signed-off-by: Ayush <mail@ayuch.dev>

--- server.go
+++ server.go
@@ -6,10 +6,13 @@ import (
 	"strings"
 )
 
+// PlayerStore stores player's information
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
+// PlayerServer is a HTTP interface for player information
 type PlayerServer struct {
 	store PlayerStore
 }
@@ -17,20 +20,28 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	fmt.Fprint(w, score)
 }
 
-// func PlayerServer(w http.ResponseWriter, r *http.Request) {
-// 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
-// 	fmt.Fprint(w, GetPlayerScore(player))
-// }
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
 
 func GetPlayerScore(name string) string {
 	if name == "Pepper" {
